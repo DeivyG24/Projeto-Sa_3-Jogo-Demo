6d9a0b3abeadcbc049bea64a4dbdf5710b466861
deivy_guzman <deivy_guzman@estudante.sesisenai.org.br>
2026-05-12 16:00:58 -0300

    teste 4

--- Assets/Scripts/teste.cs
+++ Assets/Scripts/teste.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class teste : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first execution of Update after the MonoBehaviour is created teste 1
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
